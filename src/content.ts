f716a30900b4c73d8c649dc0488c1efa3c2d44e5
Adam Murray <amurray@cloudflare.com>
2023-01-13 13:49:39 -0600

    move function into correct file. update html output.

--- src/content.ts
+++ src/content.ts
@@ -1,19 +1,28 @@
+function celToFar(temp: string) {
+  return Math.trunc((+temp * 9) / 5 + 32) + "\xB0F.";
+}
+
 function returnHtml(deviceResponse: any) {
   const devices = deviceResponse.data.devices;
   const inUse = devices.length > 0;
 
   const html = `<!DOCTYPE html>
-<body>
-  <h1>Hello!</h1>
-  <p>I'm ${inUse ? "" : "not"} currently using my Meater+.</p>
-  ${
-    inUse
-      ? `<p>Today I'm cooking a ${devices[0].cook.name.toLowerCase()} with a target temp of ${celToFar(
-          devices[0].cook.temperature.target
-        )}</p'>`
-      : ""
-  }
-</body>`;
+  <head>
+    <script defer src="https://unpkg.com/alpinejs@3.10.5/dist/cdn.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+  <body>
+    <div x-data class="container mx-auto p-2 flex rounded-sm outline outline-offset-2 outline-1 shadow-sm">
+      <p>I'm ${inUse ? "" : "not"} currently using my Meater+.</p>
+      ${
+        inUse
+          ? `<p>Today I'm cooking a ${devices[0].cook.name.toLowerCase()} with a target temp of ${celToFar(
+              devices[0].cook.temperature.target
+            )}</p'>`
+          : ""
+      }
+    </div>
+  </body>`;
   return html;
 }
 
